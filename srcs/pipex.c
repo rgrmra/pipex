8cc7b1d99cd4aecc4e84004d8f0e206e1e3b5cff
Roger de Moura <rde-mour@student.42sp.org.br>
2024-01-17 22:06:41 -0300

    Get Env

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,20 +6,39 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 18:32:45 by rde-mour          #+#    #+#             */
-/*   Updated: 2024/01/17 19:50:59 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/01/17 21:57:22 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+char	**findenv(char **env, char *var)
+{
+	size_t	i;
+
+	i = 0;
+	while (ft_strncmp(*(env + i), var, 4))
+		i++;
+	return (ft_split(*(env + i), ':'));
+}
+
+
 int	main(int argc, char **argv, char **env)
 {
-	int	i;
+	int		i;
+	char	**path;
+
 
 	if (argc < 2 || argv[1] == (void *)0)
 		return (1);
+	path = findenv(env, "PATH");
 	i = 0;
-	while (*(env + i))
-		ft_printf("%s\n", *(env + i++));
+	while (*(path + i))
+	{
+		ft_printf("%s\n", *(path + i));
+		free(*(path + i ));
+		i++;
+	}
+	free(path);
 	return (0);
 }
